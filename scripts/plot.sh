e06cb5d5a7119b5cb7fc8cfe02171e24f5761038
Eddie Hurtig <eddie@hurtigtechnologies.com>
2017-10-13 00:48:39 -0400

    Cleanup ./scripts/plot.sh and add verbose debugging

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -4,10 +4,22 @@ PLOTTING_DIR=$(realpath ./plots)
 
 git fetch origin master
 
+echo "Removing Pre-existing Plot Files"
+rm -v PLOT-*
+
+echo "FETCH_HEAD is defined as: "
+git show FETCH_HEAD
+
+echo "HEAD is defined as: "
+git show HEAD
+
+echo "Files Changed On HEAD From FETCH_HEAD"
+git diff --name-only FETCH_HEAD
+
 for filename in $(git diff --name-only FETCH_HEAD | grep kicad_pcb); do
   rm -rf $PLOTTING_DIR
-  echo "Handling $filename"
   for format in "${FORMATS[@]}"; do
+    echo "+> Generating '$format' plots for $filename"
     outdir=$PLOTTING_DIR/$format/
     mkdir -p $outdir
     ./scripts/plot_board.py $filename $outdir $format
